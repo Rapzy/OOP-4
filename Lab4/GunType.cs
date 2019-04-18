e57802d9c1afea85fa4e85ac3f82a7d674cdead3
Rapzy <andrushko.egor@gmail.com>
2019-04-18 19:56:49 +0300

    Realize IEquatable interface

--- Lab4/GunType.cs
+++ Lab4/GunType.cs
@@ -27,13 +27,31 @@ namespace Lab4
             else
                 return false;
         }
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+            GunType gunTypeObj = obj as GunType;
+            if (gunTypeObj == null)
+                return false;
+            else
+                return Equals(gunTypeObj);
+        }
+        public override int GetHashCode()
+        {
+            return Type.GetHashCode();
+        }
         public static bool operator ==(GunType gunType1, GunType other)
         {
-            return gunType1.Type == other.Type;
+            if (((object)gunType1) == null || ((object)other) == null)
+                return Object.Equals(gunType1, other);
+            return gunType1.Equals(other);
         }
         public static bool operator !=(GunType gunType1, GunType other)
         {
-            return gunType1.Type != other.Type;
+            if (gunType1 == null || other == null)
+                return ! Object.Equals(gunType1, other);
+            return ! gunType1.Equals(other);
         }
 
     }
